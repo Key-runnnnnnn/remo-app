90a03c327acd4ac5114adabddca269c1ce42d02d
adityakhade321 <adityakhadeak47@gmail.com>
2024-09-06 11:37:32 +0530

    banner and navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import { app, analytics } from './firebaseConfig'; // Import Firebase configuration
 import Navbar from "./Components/Navbar/Navbar"
+import Banner from "./Components/Banner/Banner"
 
 function App() {
   useEffect(() => {
@@ -14,6 +15,7 @@ function App() {
   return (
     <>
     <Navbar/>
+    <Banner/>
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-
+import Logo from "/logo.png"
 
 const navigation = [
-  { name: "Home", id: "Home" },
-  { name: "About", id: "About" },
-  { name: "Education", id: "Education" },
-  { name: "Skills", id: "Skills" },
-  { name: "Projects", id: "Projects" },
-  { name: "Contact", id: "Contact" },
+  { name: "Fleet", id: "Home" },
+  { name: "About Us", id: "About" },
+  { name: "Blogs", id: "Education" },
+  { name: "Careers", id: "Projects" },
+  { name: "Contact Us", id: "Contact" },
 ];
 
 const Navbar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className={` sticky top-0 z-50 px-2 py-2 bg-yellow-300`}>
-      <nav className="container mx-auto px-6 lg:px-8 flex items-center justify-between" aria-label="Global">
-         <div className="flex lg:flex-1 lg:block">Logo</div>
+    <header className="sticky top-0 z-50 p-3 bg-slate-300 text-2xl h-20">
+      <nav
+        className="container mx-auto px-6 lg:px-8 flex text-left justify-between h-16"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1 lg:block py-2">
+          <img src={Logo} alt="Logo" className="h-15 w-48" />
+        </div>
         <div className="flex items-center lg:hidden">
           <button
             type="button"
@@ -29,12 +33,11 @@ const Navbar = () => {
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
-        <div className="hidden lg:flex lg:gap-x-12">
+        <div className="hidden lg:flex lg:gap-x-12 p-2">
           {navigation.map((item) => (
             <div
               key={item.name}
-              onClick={() => scrollToSection(item.id)}
-              className="cursor-pointer text-base font-semibold leading-6"
+              className="cursor-pointer font-normal"
             >
               {item.name}
             </div>
@@ -42,7 +45,7 @@ const Navbar = () => {
         </div>
       </nav>
       <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-        <DialogPanel className={` fixed inset-0 z-50 overflow-y-auto px-6 py-6 lg:hidden`}>
+        <DialogPanel className="fixed inset-0 z-50 overflow-y-auto px-6 py-6 lg:hidden bg-slate-300 w-60">
           <div className="flex items-center justify-between">
             <button
               type="button"
@@ -55,12 +58,11 @@ const Navbar = () => {
           </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
+              <div className="space-y-2 py-6 px-3">
                 {navigation.map((item) => (
                   <div
                     key={item.name}
-                    onClick={() => scrollToSection(item.id)}
-                    className="-mx-3 cursor-pointer block rounded-lg py-2 px-3 text-base font-semibold leading-7"
+                    className="-mx-3 cursor-pointer block rounded-lg py-2 px-4 font-normal leading-5"
                   >
                     {item.name}
                   </div>
