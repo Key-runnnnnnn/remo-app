82db0c965f67b5ae8b7b39ced20a4efa0b4e7a95
adityakhade321 <adityakhadeak47@gmail.com>
2024-09-06 15:08:00 +0530

    commit zymo

--- src/Components/Zymo/Zymo.jsx
+++ src/Components/Zymo/Zymo.jsx
@@ -1,30 +1,33 @@
-import React from 'react';
-import imageIcon from "/logo.png"; // Use actual path to the icons
+import React from "react";
+import Car from '/car-4-512.png';
+import Hand from '/handshake-2-512.png';
+import Building from '/organization-512.png';
+import Users from '/conference-call-512.png';
+// import User from "../../../public/conference-call-512.ico"
+
 
 const navigation = [
-  { image: imageIcon, count: "100,000+", done: "Satisfied Users" },
-  { image: imageIcon, count: "30,000+", done: "Cars" },
-  { image: imageIcon, count: "18", done: "Service Providers" },
-  { image: imageIcon, count: "59", done: "Cities" },
+  { image: Users, count: "100,000+", done: "Satisfied Users" },
+  { image: Car, count: "30,000+", done: "Cars" },
+  { image: Hand, count: "18", done: "Service Providers" },
+  { image: Building, count: "59", done: "Cities" },
 ];
 
 const Zymo = () => {
   return (
-    <div className="text-center py-10">
+    <div className="text-center py-10 flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mb-8">Why Zymo?</h1>
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
+      <div className="grid grid-cols-2 lg:grid-cols-4 sm:gap-28 gap-16 p-10">
         {navigation.map((item, index) => (
-          <div
-            key={index}
-            className="flex flex-col items-center bg-gradient-to-br from-purple-500 to-blue-500 p-6 rounded-lg shadow-lg"
-          >
-            <img
-              src={item.image}
-              alt="icon"
-              className="w-10 h-4 mb-4"
-            />
+          <div key={index}>
+            <div
+              
+              className="flex flex-col items-center justify-center w-32 h-32 sm:w-48 sm:h-48 bg-gradient-to-r from-indigo-500 to-[#7c2a9d] p-3 sm:p-6 rounded-3xl shadow-lg"
+            >
+              <img src={item.image} alt="icon" className="w-14 h-14 mb-4 " />
+            </div>
             <p className="text-2xl font-bold">{item.count}</p>
-            <p className="text-base font-medium text-gray-200">{item.done}</p>
+            <p className="text-base font-medium text-gray-900">{item.done}</p>
           </div>
         ))}
       </div>
